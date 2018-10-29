813f629308d68b8dd65d6e81524fb4d4595e3879
me <sniper96a@gmail.com>
2018-10-28 22:45:04 -0400

    moved zapcc.sh

--- zapcc.sh
+++ zapcc.sh
@@ -1,15 +1,11 @@
-git clone https://github.com/yrnkrn/zapcc.git llvm
+git clone --depth=1 https://github.com/yrnkrn/zapcc.git llvm
 mkdir -p build
 cd build
 	cmake -G Ninja \
 	 -DCMAKE_BUILD_TYPE=Release \
 	 -DLLVM_ENABLE_WARNINGS=OFF \
-	 -DCMAKE_INSTALL_PREFIX="$pkgdir"  \
+	 -DCMAKE_INSTALL_PREFIX="/home/me/tools/zapcc"  \
 	 -DLLVM_DEFAULT_TARGET_TRIPLE=$CARCH-pc-windows-gnu  \
 	 ../llvm
 
-	ninja
-	
-	ninja check-all
-	
-	
+	ninja	
