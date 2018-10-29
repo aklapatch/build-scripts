3012afdf937d1a44c7ee0c603b4389a8ed848386
me <sniper96a@gmail.com>
2018-10-29 00:12:25 -0400

    started kernel

---
+++ lxq-kernel.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+ver="4.18.16"
+packname="v$ver-lqx1.tar.gz"
+#zen-kernel-4.18.16-lqx1
+foldername="zen-kernel-$ver-lqx1"
+wget -nc "https://github.com/zen-kernel/zen-kernel/archive/$packname"
+
+if [ ! -d "$foldername" ]; then
+	tar -xf "$packname"
+fi
+
+cd "$foldername"
+
+make menuconfig
+
+printf "Run:\n make -j4\nmake modules_install\nmake install\nsudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+# then run 'make menuconfig'
+# 'make modules_install'
+# 'make install'
+# Arch: sudo grub-mkconfig -o /boot/grub/grub.cfg
+
