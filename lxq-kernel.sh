f4677c0ee0b2902a44375cba05c1ca2f78d47c56
me <sniper96a@gmail.com>
2018-10-29 09:05:41 -0400

    added config + lqx mods

--- lxq-kernel.sh
+++ lxq-kernel.sh
@@ -2,9 +2,12 @@
 
 ver="4.18.16"
 packname="v$ver-lqx1.tar.gz"
+https://github.com/zen-kernel/zen-kernel/archive/v4.18.16-zen1.tar.gz
 #zen-kernel-4.18.16-lqx1
 foldername="zen-kernel-$ver-lqx1"
-wget -nc "https://github.com/zen-kernel/zen-kernel/archive/$packname"
+wget -nc "https://github.com/zen-kernel/zen-kernel/archive/v$ver-lqx1.tar.gz"
+
+#https://github.com/zen-kernel/zen-kernel/archive/v4.18.16-lqx1.tar.gz
 
 if [ ! -d "$foldername" ]; then
 	tar -xf "$packname"
@@ -15,9 +18,4 @@ cd "$foldername"
 make menuconfig
 
 printf "Run:\n make -j4\nmake modules_install\nmake install\nsudo grub-mkconfig -o /boot/grub/grub.cfg"
-
-# then run 'make menuconfig'
-# 'make modules_install'
-# 'make install'
-# Arch: sudo grub-mkconfig -o /boot/grub/grub.cfg
 
