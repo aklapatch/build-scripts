e379bce3dfc6e6bbbf859fa891febfa869e2fe8a
me <sniper96a@gmail.com>
2018-11-06 23:28:56 -0500

    updated scripts and test config

--- kernels/buildInstallKer.sh
+++ kernels/buildInstallKer.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-if [ ! -d "$1" ]; then
-	echo "Directory does not exist. Please specify a correct directory"
+# kernel name is arg 1
+if [ $# -lt 2 ]
+then
+	echo "Usage: $0 buildDir kernelName"
 	exit
 fi
+
+kername=$2
 
 cd "$1"
 make -j 3
 
-sudo make modules_install headers_install install
+sudo make -j 3 modules_install headers_install install
+
+#install bcm module
+sudo dkms install broadcom-wl/6.30.223.271 -k "$kername"
+
+# generate kernel initramfs
+sudo mkinitcpio -k /boot/vmlinuz -c /etc/mkinitcpio.conf -g "/boot/initramfs-$kername.img" -S autodetect
+
+# update grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Remember to set the initrd to include the intel_ucode."
